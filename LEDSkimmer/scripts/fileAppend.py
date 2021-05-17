382c98624e59d6b60d241e5020039d8e9300b6d8
littletalks13 <63623688+littletalks13@users.noreply.github.com>
2021-05-17 11:22:05 -0500

    Update fileAppend.py
     fix identation problems

--- LEDSkimmer/scripts/fileAppend.py
+++ LEDSkimmer/scripts/fileAppend.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 f1 = open("CMS_Run2011B_DoubleElectron_AOD_12Oct2013-v1_00000_file_index.txt", "r")
-f2 = open("parallel-Electron_example.yaml","a+")
+f2 = open("parallel-Electron_example2.yaml","a+")
 i = 0
 for linea in f1:
 	i = i+1
 	f2.write("- name: "+"a"+str(i)+"\n")
 	f2.write("  template: nanoaod-argo arguments: \n")
 	f2.write("  parameters: [{name: files, value: "+linea+"")
-	f2.write("},{name: it, value:"+str(i)+"}]\n ")
+	f2.write("              },{name: it, value:"+str(i)+"}]\n ")
 f1.close()
 f2.close()

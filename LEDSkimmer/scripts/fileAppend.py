8e789ef20d490872a8996ffd9895c99664592e7b
littletalks13 <63623688+littletalks13@users.noreply.github.com>
2021-05-17 10:37:19 -0500

    Update fileAppend.py
    Fix identation

--- LEDSkimmer/scripts/fileAppend.py
+++ LEDSkimmer/scripts/fileAppend.py
@@ -7,7 +7,7 @@ for linea in f1:
 	i = i+1
 	f2.write("		- name: "+"a"+str(i)+"\n")
 	f2.write("		template: nanoaod-argo arguments: \n")
-	f2.write("		parameters: [{name: files, value: "+linea+"}, {name: it, value:"+str(i)+"}]\n\n")
-		
+	f2.write("		parameters: [{name: files, value: "+linea+"}, {name: it, value:"+str(i)+"}]\n")
+	
 f1.close()
 f2.close()

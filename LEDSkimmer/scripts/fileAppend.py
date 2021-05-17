239a7e7a341814954cad99ba8b59801ae97f7292
littletalks13 <63623688+littletalks13@users.noreply.github.com>
2021-05-17 10:54:58 -0500

    Update fileAppend.py
    fix errors

--- LEDSkimmer/scripts/fileAppend.py
+++ LEDSkimmer/scripts/fileAppend.py
@@ -5,9 +5,9 @@ f2 = open("parallel-Electron_example.yaml","a+")
 i = 0
 for linea in f1:
 	i = i+1
-	f2.write("		- name: "+"a"+str(i)+"\n")
-	f2.write("		template: nanoaod-argo arguments: \n")
-	f2.write("		parameters: [{name: files, value: "+linea+"}, {name: it, value:"+str(i)+"}]\n")
-	
+	f2.write("- name: "+"a"+str(i)+"\n")
+	f2.write("  template: nanoaod-argo arguments: \n")
+	f2.write("  parameters: [{name: files, value: "+linea+"")
+	f2.write("},{name: it, value:"+str(i)+"}]\n ")
 f1.close()
 f2.close()

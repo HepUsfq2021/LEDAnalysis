f629c54a3d671f54d83ca48eb7a79d4d3687df47
Pablo Ruales <pablomartinruales@gmail.com>
2021-05-18 12:00:21 -0500

    added config files for parallel simulation

---
+++ simulation/fileAppendSimulation.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+f = open("pruebaFileAppend.yaml","a+")
+i = 0
+for i in range(140):
+	i = i+1
+	f.write("      - name: "+"a"+str(i)+"\n")
+	f.write("        template: simulation-argo\n")
+	f.write("        arguments:\n")
+	f.write("          parameters: [{name: it, value:"+str(i)+"}]\n")
+		
+f.close()

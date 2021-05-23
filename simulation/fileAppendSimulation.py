1cf66aed6dd38cf6485a2c0fc73be0742e9a4a41
pmruales <pablomartinruales@gmail.com>
2021-05-22 23:20:00 -0500

    added working yaml file for parallel data simulation/ added script for writing tasks in yaml file

--- simulation/fileAppendSimulation.py
+++ simulation/fileAppendSimulation.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-f = open("parallel-simulation_1400events.yaml","a+")
+f = open("parallel-simulation.yaml","a+")
 i = 0
-for i in range(140):
+for i in range(2000):
 	i = i+1
-	f.write("      - name: "+"a"+str(i)+"\n")
+	f.write("      - name: a"+str(i)+"\n")
 	f.write("        template: simulation-argo\n")
 	f.write("        arguments:\n")
-	f.write("          parameters: [{name: it, value:"+str(i)+"}]\n")
+	f.write("          parameters: [{name: it, value: "+str(i)+"}]\n")
 		
 f.close()

9b5be74da63299be95d0b54862fe2a38c5bf6ccb
pmruales <pablomartinruales@gmail.com>
2021-05-23 20:09:14 -0500

    'SetLogy() in analyzer.cxx

--- LEDAnalyzer/analyzer.cxx
+++ LEDAnalyzer/analyzer.cxx
@@ -117,7 +117,7 @@ for (int j=0;j<nEvents;++j){
 }
 
 TFile* outfile = new TFile("histosPrueba.root","RECREATE");
-himass->SetLog
+himass->SetLogy();
 himass->Write();
 hhovere->Write();
 himass->Print("invMassSim.png");
